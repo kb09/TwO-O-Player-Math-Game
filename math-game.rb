9e6dff71c5e867768d98e97ed095451e3a14982f
kartik <kabakshi@hotmail.ca>
2022-01-03 17:54:20 -0500

    creating game

--- math-game.rb
+++ math-game.rb
@@ -2,3 +2,12 @@
 # Isolate your I/O logic to one or two classes. Don't allow all your classes to interact (gets/puts) with the User
 # Remember to build incrementally, writing small amounts of runnable code instead of writing out a whole bunch of classes/methods that are not yet testable/runnable.
 # Use puts, raise some_val.inspect, and byebug to help with debugging
+require "./main.rb"
+
+class Game 
+  def initalize
+    puts "Enter player 1 name"
+    player1 = gets.chomp
+    puts "Enter player 2 name"
+    player2 = gets.chomp
+  end
