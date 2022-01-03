211422c0be9b78a27e114504181c2ddc52cad322
kartik <kabakshi@hotmail.ca>
2022-01-03 17:48:58 -0500

    add math-game

--- main.rb
+++ main.rb
@@ -1,7 +1,3 @@
-# Use gets.chomp to get input from users and puts for output
-# Isolate your I/O logic to one or two classes. Don't allow all your classes to interact (gets/puts) with the User
-# Remember to build incrementally, writing small amounts of runnable code instead of writing out a whole bunch of classes/methods that are not yet testable/runnable.
-# Use puts, raise some_val.inspect, and byebug to help with debugging
-
 require "./questions.rb"
 require "./player.rb"
+require "./math-game.rb"
---
+++ math-game.rb
@@ -0,0 +1,4 @@
+# Use gets.chomp to get input from users and puts for output
+# Isolate your I/O logic to one or two classes. Don't allow all your classes to interact (gets/puts) with the User
+# Remember to build incrementally, writing small amounts of runnable code instead of writing out a whole bunch of classes/methods that are not yet testable/runnable.
+# Use puts, raise some_val.inspect, and byebug to help with debugging
