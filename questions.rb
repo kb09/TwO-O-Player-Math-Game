3d80814124fd30083e38a2bab61d5bd45633d990
kartik <kabakshi@hotmail.ca>
2022-01-03 17:40:52 -0500

    adding questioins

--- questions.rb
+++ questions.rb
@@ -1,11 +1,13 @@
 class Questions
+  
+  attr_reader: newQuestion, :answer
 
   def initalize 
     num1 = rand (1..20)  # inclusive Range to generate random numbers from 1 to (including) 20 
     num2 = rand (1..20)
 
-    @newQuestion = "What does #{num1} plus #{num2} equal?"
-    @answer = num1 + num2
+    @newQuestion = "What does #{num1} plus #{num2} equal?" #asking question to user
+    @answer = num1 + num2 #correct answer
   end   
   
 end
