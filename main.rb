b76b933fcf0809d025c7783daaa774ad30a5606c
kartik <kabakshi@hotmail.ca>
2022-01-03 17:45:38 -0500

    add player info

--- main.rb
+++ main.rb
@@ -4,3 +4,4 @@
 # Use puts, raise some_val.inspect, and byebug to help with debugging
 
 require "./questions.rb"
+require "./player.rb"
---
+++ player.rb
@@ -0,0 +1,9 @@
+class Player 
+  
+  def initalize (name) # player info
+    @name = name
+    @turn = false 
+    @lives = 3
+  end 
+
+end 
